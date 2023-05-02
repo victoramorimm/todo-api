4f35993ab421c7d934deadc2432bfd294b7c3b59
Victor Amorim <victorvmrgamer@gmail.com>
2023-05-02 19:14:53 -0300

    feat: call task repository with correct values

--- src/modules/tasks/usecase/list/ListTasksUseCase.ts
+++ src/modules/tasks/usecase/list/ListTasksUseCase.ts
@@ -1,17 +1,15 @@
+import { ListTasksDTO } from "../../controller/list/IListTasksController";
+import { ITaskRepository } from "../../infra/repository/protocol/ITaskRepository";
 import { IListTasksUseCase, ListTasksUseCaseResult } from "./IListTasksUseCase";
 
 export class ListTasksUseCase implements IListTasksUseCase {
-	async execute(request: void): Promise<ListTasksUseCaseResult> {
-		const objeto = {
-			id: 1,
-			done: false,
-			task: 'Tarefa 01'
-		};
+	constructor(private readonly taskRepository: ITaskRepository) {}
 
-		const array = Array.from({ length: 50 }, () => Object.assign({}, objeto));
+	async execute(request: ListTasksDTO): Promise<ListTasksUseCaseResult> {
+		const tasks = await this.taskRepository.list(request)
 
 		return {
-			todoItem: array
+			todoItem: tasks
 		}
 	}
 }
